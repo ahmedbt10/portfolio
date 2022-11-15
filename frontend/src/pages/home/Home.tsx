b819c0ddcbaafb0391a5a5bf4097aea7b4b7f876
cannaj0int <ahmed_bt9@outlook.com>
2022-11-15 12:15:12 +0100

    added the modal form

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import  {NameContext}  from "../../context/NameContext"
 import ContactSection from "../../layout/ContactSection"
 import IntroSection from "../../layout/home layout/IntroSection"
+import FirstModal from "../../layout/home layout/Modal/FirstModal"
 import WorkSection from "../../layout/home layout/WorkSection"
 const Home = () => {
   document.getElementsByTagName("title")[0].innerText = "Ahmed Ben Tekaya-home"
+  
   return (
-    <main id="main-home">
-      <IntroSection/>
-      <WorkSection/>
-      <ContactSection/>
-    </main>
+      <main id="main-home">
+        <IntroSection/>
+        <WorkSection/>
+        <ContactSection/>
+      </main>
   )
 }
 
