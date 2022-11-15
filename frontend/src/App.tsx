dad1fe9d714d6b51b6b0c5bda36acca56afaf3ac
cannaj0int <ahmed_bt9@outlook.com>
2022-11-15 12:06:41 +0100

    added the context provider to pass the name through all components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,36 @@
+import { useState } from "react"
 import {Routes,Route} from "react-router-dom"
+import { NameContext } from "./context/NameContext"
 import Footer from "./layout/footer/Footer"
 import Header from "./layout/header/Header"
+import FirstModal from "./layout/home layout/Modal/FirstModal"
 import { About } from "./pages/about/About"
 import ContactMe from "./pages/contact-me/ContactMe"
 import Home from "./pages/home/Home"
 
 function App() {
-  
+  const [name, setName]:[string,React.Dispatch<React.SetStateAction<string>>] = useState("")
   return (
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/contact" element={<ContactMe />}/>
-        </Routes>
-        <Footer/>
-      </div>
+      
+      
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<>
+        <FirstModal setName={setName}/>
+        <NameContext.Provider value={name}>
+          <Home />
+        </NameContext.Provider>
+      </>}/>
+            <Route path="/about" element={<NameContext.Provider value={name}>
+          <About />
+        </NameContext.Provider>}/>
+            <Route path="/contact" element={<NameContext.Provider value={name}>
+          <ContactMe />
+        </NameContext.Provider>}/>
+          </Routes>
+          <Footer/>
+        </div>
   )
 }
 
